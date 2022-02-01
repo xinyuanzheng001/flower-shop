ed0e73caf3e332d690868ccab04999dbe558429f
xinyuanzheng <xinyuazheng1995@gmail.com>
2022-02-01 13:54:59 -0500

    fix error

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -9,7 +9,7 @@ const userSchema = mongoose.Schema(
     },
     phoneNumber: {
       type: String,
-      required: true,
+      // required: true,
     },
     email: {
       type: String,
--- back-end/seeder.js
+++ back-end/seeder.js
@@ -14,45 +14,44 @@ dotenv.config()
 connectDB()
 
 const destroyData = async () => {
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-
-        console.log('Data Destroyed!')
-        process.exit()
-    } catch (error) {
-        console.error(`${error}`)
-        process.exit(1)
-    }
+    console.log('Data Destroyed!')
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`)
+    process.exit(1)
+  }
 }
 const importData = async () => {
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        const createdUsers = await User.insertMany(users)
+    const createdUsers = await User.insertMany(users)
 
-        const adminUser = createdUsers[0]._id
+    const adminUser = createdUsers[0]._id
 
-        const sampleProduct = products.map(product => {
-            return { ...product, user: adminUser }
-        })
+    // const sampleProduct = products.map(product => {
+    //     return { ...product, user: adminUser }
+    // })
 
-        await Product.insertMany(sampleProduct)
+    // await Product.insertMany(sampleProduct)
 
-        console.log('Data Imported!')
-        process.exit()
-    } catch (error) {
-        console.error(`${error}`)
-        process.exit(1)
-    }
+    console.log('Data Imported!')
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`)
+    process.exit(1)
+  }
 }
 
 if (process.argv[2] === '-d') {
-    destroyData()
+  destroyData()
 } else {
-    importData()
+  importData()
 }
